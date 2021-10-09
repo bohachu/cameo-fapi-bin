bca1d1e2df49b683d7e832494bd64559630e2748
bohachu <cbh@cameo.tw>
2021-10-09 17:36:29 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -e
+if [ "$OS" = "Windows_NT" ]; then
+	target="x86_64-pc-windows-msvc"
+else
+	case $(uname -sm) in
+	"Darwin x86_64") target="x86_64-apple-darwin" ;;
+	"Darwin arm64") target="aarch64-apple-darwin" ;;
+	*) target="x86_64-unknown-linux-gnu" ;;
+	esac
+fi
+
+cameo_uri="https://fapi.cameo.tw/download/cameo-fapi-${target}.zip"
+
+cameo_install="${CAMEO_INSTALL:-$HOME/.cameo}"
+bin_dir="$cameo_install/bin"
+exe="$bin_dir/cameo-fapi"
+
+if [ ! -d "$bin_dir" ]; then
+	mkdir -p "$bin_dir"
+fi
+
+curl --fail --location --progress-bar --output "$exe.zip" "$cameo_uri"
+unzip -d "$bin_dir" -o "$exe.zip"
+chmod +x "$exe"
+rm "$exe.zip"
+
+echo "cameo-fapi was installed successfully to $exe"
+if command -v cameo-fapi >/dev/null; then
+	echo "Run 'cameo-fapi --help' to get started"
+else
+	case $SHELL in
+	/bin/zsh) shell_profile=".zshrc" ;;
+	*) shell_profile=".bash_profile" ;;
+	esac
+	echo "Manually add the directory to your \$HOME/$shell_profile (or similar)"
+	echo "  export DENO_INSTALL=\"$cameo_install\""
+	echo "  export PATH=\"\$CAMEO_INSTALL/bin:\$PATH\""
+	echo "Run '$exe --help' to get started"
+fi
